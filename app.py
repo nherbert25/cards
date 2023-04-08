cb1875f57af0518a3adb86eb0735b6d2f065ea96
Nate Herbert <nate.herbert@self.inc>
2023-04-07 21:32:54 -0500

    added buttons :)

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+#@app.route('/')
 def hello_world():  # put application's code here
     print('testing')
     print('pls work')
     print('test3')
     return 'Hello World!'
+
+
+@app.route('/') # this defines the entrance to your code. my_website.com goes HERE as well as my_website.com/home
+@app.route('/home')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    output = request.form.to_dict()
+    print(output)
+    name = output["name"]
+
+    return render_template('index.html', name=name)
 
 
 if __name__ == '__main__':
