e9a09dff43a7fa7f66c1693e536ee6de39077f92
dojiherbs <nateherbert@gmail.com>
2023-04-29 14:12:36 -0500

    figured out how to update the page

--- black_jack/black_jack.py
+++ black_jack/black_jack.py
@@ -1,8 +1,36 @@
+from random import shuffle as r_shuffle
 
+player_name = 'Taylor'
 dealer_sum = 1
 your_sum = 0
 
 dealer_ace_count = 0
 your_ace_count = 0
 
+
+class Card:
+    """
+    Card class that represents each card in the deck.
+
+    Attributes:
+        rank (str): Represents the point value of each card (2 - Ace).
+        suit (str): Represents the suit of each card (Hearts, Diamonds, Clubs, Spades).
+        image (str): Represents the filepath to each card image that is displayed on the page.
+
+    """
+
+    def __init__(self, rank, suit, image=None):
+        self.rank = rank
+        self.suit = suit
+        self.image = image
+
+
+class Deck:
+    def __init__(self):
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
+        self.cards = [Card(rank, suit) for rank in ranks for suit in suits]
+
+    def shuffle(self):
+        r_shuffle(self.cards)
 
--- black_jack/routes.py
+++ black_jack/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 import black_jack.black_jack as bj
 
 black_jack_blueprint = Blueprint('black_jack', __name__)
@@ -6,8 +6,14 @@ black_jack_blueprint = Blueprint('black_jack', __name__)
 
 @black_jack_blueprint.route('/black_jack')
 def black_jack():
-    return render_template("black_jack.html", dealer_sum=bj.dealer_sum)
+    deck = bj.Deck()
+    deck.shuffle()
+    return render_template("black_jack.html", dealer_sum=bj.dealer_sum, player_name=bj.player_name)
 
-@black_jack_blueprint.route('/hit')
+
+@black_jack_blueprint.route('/hit', methods=['GET'])
 def hit():
-    pass
+    print('testing')
+    bj.dealer_sum += 1
+    return redirect(url_for('black_jack.black_jack'))
+
