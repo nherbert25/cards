096464ae301232b1d50f6ddc568d768d64190eda
dojiherbs <nateherbert@gmail.com>
2023-06-17 17:05:33 -0500

    minor comments

--- static/black_jack.js
+++ static/black_jack.js
@@ -6,11 +6,13 @@
 //let e = new Date();
 //document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
 
-// https://www.youtube.com/watch?v=4K33w-0-p2c   good video on xml http requests
+// https://www.youtube.com/watch?v=4K33w-0-p2c   good video on xml http requests  (callbacks?)
 // https://www.youtube.com/watch?v=23hrM4saaMk    rebuilding the last video with the fetch api instead (it's more modern)
 //    look up async/await. I *think* they're built on top of promises. They're a wrapper that makes the code more readable, yet under the hood they're still promises.
 // from my 1 second google, you still write your promise, then your other function will be defined as an async function by adding it as a keyword
 // in the definition, aka: "async function myFunction() { ...my code ..  await myPromise() ... my code ..  }"
+// https://www.youtube.com/watch?v=AMp6hlA8xKA   using websockets instead of fetch and callbacks
+
 
 var dealer_sum = 0;
 var your_sum = 0;
