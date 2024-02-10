e29996817450ca8c4c97bcca49d31e1fba262ca4
Nate Herbert <nateherbert@gmail.com>
2024-02-10 13:12:25 -0600

    change worker count

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,4 @@
-workers = 1  # Adjust the number of workers based on your application's needs
+workers = 3  # Adjust the number of workers based on your application's needs
 timeout = 300  # Set a reasonable timeout value.
 
 # to run: "gunicorn -c gunicorn_config.py app:app"
