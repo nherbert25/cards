8a385b739389bca1248ceff4fd5bbc407c0be691
Nate Herbert <nateherbert@gmail.com>
2024-02-13 14:04:02 -0600

    update gunicorn config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,6 @@
 workers = 1  # Adjust the number of workers based on your application's needs. Gunicorn can only support 1 worker for web sockets! See https://flask-socketio.readthedocs.io/en/latest/deployment.html#gunicorn-web-server for details
 timeout = 300  # Set a reasonable timeout value.
+worker_class = 'eventlet'
 
 # to run: "gunicorn -c gunicorn_config.py app:app"
 # to run with eventlet "gunicorn --worker-class eventlet -w 1 app:app"
