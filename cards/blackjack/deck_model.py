d5848cb196ce127738b19cca5e9c19f120d5aa26
Nate Herbert <nateherbert@gmail.com>
2024-12-10 09:35:20 -0600

    added blank space

--- cards/blackjack/deck_model.py
+++ cards/blackjack/deck_model.py
@@ -1,6 +1,7 @@
 from random import shuffle as r_shuffle
 from cards.blackjack.card_model import Card
 
+
 class Deck:
     def __init__(self):
         ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
