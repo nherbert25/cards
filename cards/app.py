910353d6d65cb4030770cfe7ce635962d1301b56
Nate Herbert <nateherbert@gmail.com>
2025-03-01 10:42:38 -0600

    add todo

--- cards/app.py
+++ cards/app.py
@@ -3,7 +3,7 @@ from cards.app_setup import create_app, socketio
 # Create app using the factory function
 app = create_app()
 
-
+# todo: remove allow_unsafe_werkzeug for prod
 if __name__ == '__main__':
     # Run the app with SocketIO support
     socketio.run(app, allow_unsafe_werkzeug=True)
