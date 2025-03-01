5c7a01996ffae3992eaa1731dda33c39eda791f8
Nate Herbert <nateherbert@gmail.com>
2025-03-01 11:10:38 -0600

    fix bcrypt import to landing Page

--- cards/landing_page_blueprint.py
+++ cards/landing_page_blueprint.py
@@ -1,13 +1,8 @@
 from flask import render_template, request, redirect, url_for, flash, Blueprint
-# todo: I think this should be from bcrypt from app_setup
-from flask_bcrypt import check_password_hash
-from cards.app_setup import create_app, socketio, db, bcrypt
+from cards.app_setup import db, bcrypt
 from cards.forms import RegistrationForm, LoginForm
 from cards.database.models import User
 from cards.database.user_table_DAO import UserTableDAO
-
-# # Creating application
-# app = create_app()
 
 # initializing local class objects
 user_table_dao = UserTableDAO(db.session)
@@ -63,7 +58,7 @@ def login():
             db_user = user_table_dao.get_user_by_email(email=form.email.data)
 
             # TODO: make check_password_hash pull from user_table_dao.verify_password
-            if db_user is not None and check_password_hash(db_user.password, form.password.data):
+            if db_user is not None and bcrypt.check_password_hash(db_user.password, form.password.data):
                 flash('You have been logged in!', 'success')
                 return redirect(url_for('landing_page.home')), 302
             else:
