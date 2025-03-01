56852a64905bafa837ca76922408060bb91ad3d0
Nate Herbert <nateherbert@gmail.com>
2025-03-01 11:14:52 -0600

    add typing to user_table_DAO

--- cards/database/user_table_DAO.py
+++ cards/database/user_table_DAO.py
@@ -8,16 +8,16 @@ class UserTableDAO:
     def __init__(self, db_session: Session = None):
         self.db_session = db_session or db.session
 
-    def get_user_by_username(self, username: str):
+    def get_user_by_username(self, username: str) -> User | None:
         return self.db_session.query(User).filter_by(username=username).first()
 
-    def get_user_by_email(self, email: str):
+    def get_user_by_email(self, email: str) -> User | None:
         return self.db_session.query(User).filter_by(email=email).first()
 
     def verify_password(self, user: User, password: str) -> bool:
         return bcrypt.check_password_hash(user.password, password)
 
-    def add_user_to_database(self, user: User):
+    def add_user_to_database(self, user: User) -> None:
         self.db_session.add(user)
         self.db_session.commit()
         return
