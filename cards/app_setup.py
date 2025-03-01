b0b289ec38f2dc2530d9779f7f5bb1c2b7255d8c
Nate Herbert <nateherbert@gmail.com>
2025-02-28 19:41:23 -0600

    make initialization of flask extensions into separate functions

--- cards/app_setup.py
+++ cards/app_setup.py
@@ -6,14 +6,13 @@ import os
 from cards.database.models import db
 from cards.configs.config import DevelopmentConfig, TestingConfig, ProductionConfig
 
-
 # Initialize extensions
 bcrypt = Bcrypt()
 sess = Session()
 socketio = SocketIO(ping_interval=50, ping_timeout=50)
 
 
-# create app pattern:  https://flask.palletsprojects.com/en/3.0.x/patterns/appfactories/
+# application factory pattern:  https://flask.palletsprojects.com/en/3.0.x/patterns/appfactories/
 def create_app():
     """Create an application."""
     app = Flask(__name__)
@@ -27,24 +26,38 @@ def create_app():
     else:
         app.config.from_object(DevelopmentConfig)
 
-    # Load database configurations
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
-
-    # Load Session configurations
-    app.config['SESSION_SQLALCHEMY'] = db
-    app.config['SESSION_TYPE'] = 'sqlalchemy'
-    sess.init_app(app)
-
-    # Load bcrypt configurations
-    bcrypt.init_app(app)
-
-
-    socketio.init_app(app)
+    initialize_db(app)
+    initialize_session(app, db)
+    initialize_bcrypt(app)
+    initialize_socketio(app)
 
     # Load blueprints, these must be imported here to avoid circular imports. See Flask documentation
     from cards.blackjack.routes import blackjack_blueprint
     app.register_blueprint(blackjack_blueprint)
 
     return app, db, sess, bcrypt
+
+
+def initialize_db(app):
+    """Initialize Database."""
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+
+def initialize_session(app, session_db):
+    """Initialize Session."""
+    # Load Session configurations
+    app.config['SESSION_SQLALCHEMY'] = session_db
+    app.config['SESSION_TYPE'] = 'sqlalchemy'
+    sess.init_app(app)
+
+
+def initialize_bcrypt(app):
+    """Initialize Bcrypt."""
+    bcrypt.init_app(app)
+
+
+def initialize_socketio(app):
+    """Initialize SocketIO."""
+    socketio.init_app(app)
