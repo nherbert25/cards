0b50a2c14caa948346b8da90aa92e32df65af761
Nate Herbert <nateherbert@gmail.com>
2025-03-28 22:08:07 -0500

    first pass

--- cards/landing_page_blueprint.py
+++ cards/landing_page_blueprint.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for, flash, Blueprint
-from cards.app_setup import db, bcrypt
+from cards.app_setup import db, bcrypt, session
 from cards.forms import RegistrationForm, LoginForm
 from cards.database.models import User
 from cards.database.user_table_DAO import UserTableDAO
@@ -63,6 +63,7 @@ def login():
             db_user = user_table_dao.get_user_by_email(email=form.email.data)
 
             if db_user is not None and user_table_dao.verify_password(db_user.password, form.password.data):
+                session['user_uuid'] = db_user.user_uuid
                 flash('You have been logged in!', 'success')
                 return redirect(url_for('landing_page.home')), 302
             else:
